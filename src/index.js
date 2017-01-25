ffe1ee99cffb0faef5b25fb5b761c054318573d2
Ivo Roberto <byivo@byivo-personal.byivo.rocks>
2017-01-25 00:43:36 -0200

    added commander working functions and descriptions. Prepared to verbose with winston

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
 #!/usr/bin/env node
 'use strict';
+var winston = require('winston');
+var program = require('commander');
+var pkg = require('./../package.json');
+var chalk = require('chalk');
 
-const program = require('commander'),
-        chalk = require("chalk"),
-        pkg = require('./../package.json');
-
-var handler = function(directory, options) {
-  console.log(options.envyronment);
-  console.log(options.template);
-  console.log(options.customProperties);
-}
+var workingDirectory = __dirname;
+var globalValues = [];
+program
+  .version(pkg.version);
 
 program
-  .command('dc-pedidovenda')
-  .version('pkg.version')
-  .option('-e', '--envyronment <required>', 'O caminho do arquivo de ambiente deve ser informado')
-  .option('-t', '--template <required>', 'Um template base deve ser informado. Ele deve estar nos padrões de variáveis globais do PostMan e possuir uma propriedade chamada\'ordem_solicitacao\', este qual define o flow das chamadasd dos recursos no Rest. ')
-  .option('-c', '--customProperties [optional]', 'Pode-se passar ao programa uma relação de N parâmetros seguindo o padrão \'chave=valor\' (respeite o \'=\' como separador) para serem substituídos como variáveis globais pelo newman. Qualquer dúvida, busque pela documentação do DC-PEDIDOVENDA')
-  .action(handler);
+  .command('globals [globals...]', 'Global parameters should be \'=\' separated like \'key=value\'')
+  .action(function (globals) {
+    globalValues = globals;
+  });
 
-program.parse(process.argv);
+program
+  .option('-e, --envyronment <path>', 'Exported Envyronment path exported by postman. It\'s relative to working-directory path')
+  .option('-t, --template <path>', 'Template path to be filled with global values input. It\'s relative to working-directory path')
+  .option('-w, --working-directory <path>', 'Working directory to find template and envyronment. \'__dirname\' is used as default.')
+  .option('-v, --verbose', 'Verbose this execution stream')
+  .parse(process.argv);
 
-if( program.args.length === 0) program.help();
+if(program.verbose) {
+  winston.level = 'verbose';
+  winston.log('verbose', chalk.green('Verbose is enabled! Have a good time trying to find a unusual behavior.'));
+}
+
+if(program.workingDirectory) workingDirectory = program.workingDirectory;
+
+winston.log('verbose', chalk.yellow('Using ' + workingDirectory + ' as working-directory'));
+
+if(program.envyronment) winston.log('verbose', chalk.yellow('Envyronment\'s full path is ' + workingDirectory + program.envyronment));
+if(program.template) winston.log('verbose', chalk.yellow('Template\'s full path is ' + workingDirectory + program.template));
+
+if(!program.envyronment || !program.template) {
+  winston.log('error', 'Cannot procces without an envyronment and template. See references under repository ' + pkg.repository.url);
+  program.help();
+  process.exit(1);
+}
