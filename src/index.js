3306397d3115acad905f62d577bf9a1df85b3bfa
Ivo Roberto Batistela <byivo@ivo.batistela>
2017-01-25 17:08:41 -0200

    added new option to supply postman colections to newman

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ var pkg = require('./../package.json');
 var chalk = require('chalk');
 
 var workingDirectory = __dirname;
+var collection;
+var envyronment;
+var template;
 var globalValues = [];
+
 program
   .version(pkg.version);
 
@@ -17,11 +21,11 @@ program
   });
 
 program
+  .option('-v, --verbose', 'Verbose this execution stream')
   .option('-e, --envyronment <path>', 'Exported Envyronment path exported by postman. It\'s relative to working-directory path')
   .option('-t, --template <path>', 'Template path to be filled with global values input. It\'s relative to working-directory path')
   .option('-w, --working-directory <path>', 'Working directory to find template and envyronment. \'__dirname\' is used as default.')
-  .option('-v, --verbose', 'Verbose this execution stream')
-  //TODO ADD  COLLECTIONS PARAM - ITS NEEDED TO POSTMAN RUN IT
+  .option('-c, --collection <path>', 'Postman exported collection to be used as newman\'s input')
   .parse(process.argv);
 
 if(program.verbose) {
@@ -35,9 +39,10 @@ winston.log('verbose', chalk.yellow('Using ' + workingDirectory + ' as working-d
 
 if(program.envyronment) winston.log('verbose', chalk.yellow('Envyronment\'s full path is ' + workingDirectory + program.envyronment));
 if(program.template) winston.log('verbose', chalk.yellow('Template\'s full path is ' + workingDirectory + program.template));
+if(program.collection) winston.log('verbose', chalk.yellow('Collections\'s path is ' + workingDirectory + program.collection));
 
-if(!program.envyronment || !program.template) {
-  winston.log('error', 'Cannot procces without an envyronment and template. See references under repository ' + pkg.repository.url);
+if(!program.envyronment || !program.template || !program.collection) {
+  winston.log('error', 'Cannot procced without an envyronment, template and collection path. See references under repository ' + pkg.repository.url);
   program.help();
   process.exit(1);
 }
